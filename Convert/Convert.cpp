477d5665b07de492fba997d62d7618a2021c9640
Shadowsith <shadowsith94@gmail.com>
2018-03-12 20:51:52 +0100

    toInt and toBool methods added

--- Convert/Convert.cpp
+++ Convert/Convert.cpp
@@ -1,1 +1,102 @@
+#include <iostream>
+#include <exception> 
+#include <string>
+#include <regex> 
+#include <cstdlib>
+#include <cmath>  
+#include <limits>
+#include "Convert.h"
+
+bool Convert::toBool(short s) {
+    if (s == 1) {
+        return true;
+    }
+    else {
+        return false; 
+    }
+}
+
+bool Convert::toBool(int i) {
+    if (i == 1) {
+        return true;
+    }
+    if ( i == 0 ) {
+        return false;
+    }
+}
+
+bool Convert::toBool(long l) {
+    if (l == 1) {
+        return true;
+    }
+    if ( l == 0 ) {
+        return false; 
+    }
+}
+
+bool Convert::toBool(char c) {
+    if ( c == '0' || c == 'f' || c == 'F' ) {
+        return false;
+    }
+    if ( c == '1' || c == 't' || c == 'T' ) {
+        return true; 
+    }
+}
+
+bool Convert::toBool(const char* cstr) {
+    if ( cstr == "0" || cstr == "false" || cstr == "FALSE" ) {
+        return false;
+    }
+    if ( cstr == "1" || cstr == "true" || cstr == "TRUE" ) {
+        return true; 
+    }
+}
+
+bool Convert::toBool(std::string str) {
+    if ( str == "0" || str == "false" || str == "FALSE" ) {
+        return false;
+    }
+    if ( str == "1" || str == "true" || str == "TRUE" ) {
+        return true; 
+    }
+}
+
+
+int Convert::toInt(char c) {
+    return static_cast<int>(c);
+}
+
+int Convert::toInt(short s) {
+    return static_cast<int>(s);
+}
+
+int Convert::toInt(long l) {
+    try {
+        int converter = static_cast<int>(l);
+    }
+    catch(std::exception ex){
+        std::cout << "Bad conversion from long to int: ";
+        std::cout << ex.what() << std::endl;
+    }
+}
+
+int Convert::toInt(float f) {
+    f = roundf(f);
+    return static_cast<int>(f);
+} 
+
+int Convert::toInt(double d) {
+    d = round(d); 
+    return static_cast<int>(d);
+}
+
+int Convert::toInt(const char* cstr) {
+    return std::atoi(cstr); 
+}
+
+int Convert::toInt(std::string str) {
+    return std::stoi(str);
+}
+
+
 
--- Convert/Convert.h
+++ Convert/Convert.h
@@ -1,8 +1,26 @@
 #ifndef CONVERT_H
 #define CONVERT_H
 
+#include <string>
+
 class Convert {
 
+    public:
+
+        static bool toBool(short s);
+        static bool toBool(int i);
+        static bool toBool(long l); 
+        static bool toBool(char c);
+        static bool toBool(const char* cstr);
+        static bool toBool(std::string str);
+
+        static int toInt(char c);
+        static int toInt(short s);
+        static int toInt(long l);
+        static int toInt(float f);
+        static int toInt(double d);
+        static int toInt(const char* cstr); 
+        static int toInt(std::string str);
 
 };
 
