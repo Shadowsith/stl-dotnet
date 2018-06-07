466dd0d4c391d96de8f9cf7571b6c856335948a7
Shadowsith <shadowsith94@gmail.com>
2018-06-07 12:34:01 +0200

    methods in Convert class added

--- Convert/Convert.cpp
+++ Convert/Convert.cpp
@@ -61,7 +61,7 @@ bool Convert::toBool(std::string str) {
     }
 }
 
-
+// to integer
 int Convert::toInt(char c) {
     return static_cast<int>(c);
 }
@@ -73,20 +73,19 @@ int Convert::toInt(short s) {
 int Convert::toInt(long l) {
     try {
         int converter = static_cast<int>(l);
+        return converter; 
     }
-    catch(std::exception ex){
+    catch(std::exception ex) {
         std::cout << "Bad conversion from long to int: ";
         std::cout << ex.what() << std::endl;
     }
 }
 
 int Convert::toInt(float f) {
-    f = roundf(f);
     return static_cast<int>(f);
 } 
 
 int Convert::toInt(double d) {
-    d = round(d); 
     return static_cast<int>(d);
 }
 
@@ -98,5 +97,80 @@ int Convert::toInt(std::string str) {
     return std::stoi(str);
 }
 
+// to long integer
+long Convert::toLong(char c) {
+    return static_cast<long>(c);
+}
+
+long Convert::toLong(short s) {
+    return static_cast<long>(s);
+}
+
+long Convert::toLong(int i) {
+    return static_cast<long>(i);
+}
+
+long Convert::toLong(float f) {
+    return static_cast<long>(f);
+} 
+
+long Convert::toLong(double d) {
+    return static_cast<long>(d);
+}
+
+long Convert::toLong(const char* cstr) {
+    return std::atol(cstr); 
+}
+
+long Convert::toLong(std::string str) {
+    return std::stol(str);
+}
 
 
+
+// to String
+std::string Convert::toString(char c) {
+    std::string str(1, c);
+    return str;
+}
+
+std::string Convert::toString(short s) {
+    return std::to_string(s);    
+}
+
+std::string Convert::toString(int i) {
+    return std::to_string(i);    
+}
+
+std::string Convert::toString(unsigned int ui) {
+    return std::to_string(ui);    
+}
+
+std::string Convert::toString(long d) {
+    return std::to_string(d);
+}
+
+std::string Convert::toString(unsigned long ul) {
+    return std::to_string(ul);
+}
+
+std::string Convert::toString(long long ll) {
+    return std::to_string(ll);
+}
+
+std::string Convert::toString(unsigned long long ull) {
+    return std::to_string(ull);
+}
+
+std::string Convert::toString(float f) {
+    return std::to_string(f);
+}
+
+std::string Convert::toString(double d) {
+    return std::to_string(d);
+}
+
+std::string Convert::toString(const char* cstr) {
+    std::string str(cstr);
+    return str;
+}
--- Convert/Convert.h
+++ Convert/Convert.h
@@ -22,6 +22,50 @@ class Convert {
         static int toInt(const char* cstr); 
         static int toInt(std::string str);
 
+        static long toLong(char c);
+        static long toLong(short s);
+        static long toLong(int i);
+        static long toLong(float f);
+        static long toLong(double d);
+        static long toLong(const char* cstr); 
+        static long toLong(std::string str);
+
+        static float toFloat(char c);
+        static float toFloat(short s);
+        static float toFloat(int i);
+        static float toFloat(long l);
+        static float toFloat(double d);
+        static float toFloat(const char* cstr); 
+        static float toFloat(std::string str);
+
+        static double toDouble(char c);
+        static double toDouble(short s);
+        static double toDouble(int i);
+        static double toDouble(long l);
+        static double toDouble(float f);
+        static double toDouble(const char* cstr); 
+        static double toDouble(std::string str);
+
+        static const char* toCString(char c);
+        static const char* toCString(short s);
+        static const char* toCString(int i);
+        static const char* toCString(long l);
+        static const char* toCString(float f);
+        static const char* toCString(double d);
+        static const char* toCString(std::string str);
+
+        static std::string toString(char c);
+        static std::string toString(short s);
+        static std::string toString(int i);
+        static std::string toString(unsigned int ui);
+        static std::string toString(long l);
+        static std::string toString(unsigned long ul);
+        static std::string toString(long long ll);
+        static std::string toString(unsigned long long ull);
+        static std::string toString(float f);
+        static std::string toString(double d);
+        static std::string toString(long double ld);
+        static std::string toString(const char* cstr); 
 };
 
 #endif
